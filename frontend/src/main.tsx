ae4dec81c851f0538781b5fa0241b9f2594a2f20
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-13 19:34:53 -0400

    added the provider in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,14 +5,17 @@ import App from './App.tsx';
 import { SocketProvider } from './contexts/useSocket.tsx';
 import { GameProvider } from './contexts/useGameState.tsx';
 import { ChatInputProvider } from './contexts/ChatInput.tsx';
+import { VotingContextProvider } from './contexts/Voting.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
 		<SocketProvider>
 		<GameProvider>
+		<VotingContextProvider>
 		<ChatInputProvider>
 			<App />
 		</ChatInputProvider>
+		</VotingContextProvider>
 		</GameProvider>
 		</SocketProvider>
     </StrictMode>,
