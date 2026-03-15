e90112366cb75b0b012ddaad6f996b6fcd8c5257
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-15 19:13:35 -0400

    added toggle for the menu buttons

--- frontend/src/components/interface/GameLobby/LobbyMenu.tsx
+++ frontend/src/components/interface/GameLobby/LobbyMenu.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useSocket } from "../../../contexts/useSocket";
 import { scroll_reveal } from "../../../utils/consts/ScrollReveal";
+import SettingsMenu from "./SettingsMenu";
 
 const LobbyMenu = () => {
     const { socket } = useSocket();
-    const { isSettingsVisible, setIsSettingsVisible } = useState<boolean>(false);
+    const [ isSettingsVisible, setIsSettingsVisible ] = useState<boolean>(false);
+    const [ isMusicOn, setIsMusicOn ] = useState<boolean>(true);
 
     const handlePlayerExit = (e: React.MouseEvent) => {
         e.preventDefault();
@@ -15,14 +17,16 @@ const LobbyMenu = () => {
 
     const handleMusicToggle = (e: React.MouseEvent) => {
         e.preventDefault();
+        setIsMusicOn(prev => !prev);
 
         // some logic for toggling music. This could prob be some global state
     }
 
     const handleSettingsToggle = (e: React.MouseEvent) => {
         e.preventDefault();
+        setIsSettingsVisible(prev => !prev);
 
-        // some logic for toggling music. This could prob be some global state
+        // some logic for saving settings. This could prob be some global state
     }
 
     useEffect(() => {
@@ -31,11 +35,13 @@ const LobbyMenu = () => {
 
     return (
         <div className="lobby_menu_container">
-            <button onClick={() => handlePlayerExit} className="menu_button"></button>
-            <button onClick={() => handleMusicToggle} className="menu_button"></button>
-            <button onClick={() => handleSettingsToggle} className="menu_button"></button>
+            <button onClick={() => handlePlayerExit} className="menu_button exit_toggle"></button>
+            <button onClick={() => handleMusicToggle} className={`menu_button music_toggle ${!isMusicOn ? 'muted_music_button' : ''}`}></button>
+            <button onClick={() => handleSettingsToggle} className={`menu_button settings_toggle`}></button>
 
-            {}
+            {isSettingsVisible && (
+                <SettingsMenu toggleSettings={handleSettingsToggle} />
+            )}
         </div>
     );
 }
--- frontend/src/components/interface/GameLobby/SettingsMenu.tsx
+++ frontend/src/components/interface/GameLobby/SettingsMenu.tsx
@@ -1,13 +1,19 @@
-const SettingsMenu = () => {
+import '../../../styles/settings_menu.css';
+
+interface SettingsMenuProps {
+    toggleSettings: (e: React.MouseEvent) => void; 
+}
+
+const SettingsMenu = ({ toggleSettings } : SettingsMenuProps) => {
     // this will prob utilize a global state
     // so these settings can be saved throughout the game
     // but these are basic visuals for now
 
     return (
         <div className='settings_menu'>
-        
+            <h1 className='header1'>Game Settings</h1>
         </div>
-    )
+    );
 }
 
-export default SettingsMenu
+export default SettingsMenu;
