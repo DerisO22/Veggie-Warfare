e018ab112d20e1096e840f0b39dc3bb257ee9fa7
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-04-09 18:14:06 -0400

    Added other light mode but the higher performant one is dealing with shadow acne

--- frontend/src/components/scene/Lights.tsx
+++ frontend/src/components/scene/Lights.tsx
@@ -1,28 +1,52 @@
 import { useLightMode } from "../../contexts/game/LightContext";
 
+export const LightConfigs = {
+    mode0: {
+        ambient_light_intensity: 2,
+        direction_light_intensity: 0,
+        point_light_intensity: 0,
+        castShadow: false
+    },
+    mode1: {
+        ambient_light_intensity: 0.4,
+        direction_light_intensity: 1.5,
+        point_light_intensity: 0,
+        castShadow: true
+    },
+    mode2: {
+        ambient_light_intensity: 3,
+        direction_light_intensity: 0,
+        point_light_intensity: 100000,
+        castShadow: true
+    },
+}
+
 const Lights = () => {
     const { lightMode } = useLightMode();
+
+    const config = LightConfigs[`mode${lightMode}`] || LightConfigs.mode0;
     
     return (
         <>
             <ambientLight
-                intensity={0.4}
+                intensity={config.ambient_light_intensity}
             />
             
             {/* Fill light for daytime */}
             <directionalLight 
-                position={[0, 10, -30]}
-                castShadow 
-                intensity={1.5} 
-                shadow-camera-left={-100}  
-                shadow-camera-right={100}
-                shadow-camera-top={400}
-                shadow-camera-bottom={-400}
+                position={[0, 30, -200]}
+                castShadow={config.castShadow}
+                intensity={config.direction_light_intensity || 0} 
+                shadow-camera-left={-20}  
+                shadow-camera-right={20}
+                shadow-camera-top={20}
+                shadow-camera-bottom={-20}
                 shadow-camera-far={500}    
                 shadow-mapSize={[2048, 2048]} 
+                shadow-bias={0.001}
             />
 
-            {/* <pointLight castShadow position={[-20, 100, 0]} intensity={1000000.5}/> */}
+            <pointLight castShadow={config.castShadow} position={[-20, 100, -20]} intensity={config.point_light_intensity || 0}/>
         </>
     )
 }
