bf92f97be872da19fe5be5281c5996420a50e3f1
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-04-15 14:01:40 -0400

    Adjusted .env.example

--- server/database/config.js
+++ server/database/config.js
@@ -3,16 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PG_PROD_CONFIG = {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT || 5432,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    user: process.env.PGUSER,
-    max: 24,
-    idleTimeoutMillis: 500000,
-}
-
 // Just railway. No need to use local PG database on pg client
 export const createClient = () => {
     return new pg.Client({
