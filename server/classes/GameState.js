0efd045f690e2a7b800957bbb3a3621fb4a5b33a
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-04-27 22:38:41 -0400

    final game loop timings

--- server/classes/GameState.js
+++ server/classes/GameState.js
@@ -20,7 +20,7 @@ export class GameState {
 
         // 20 secs to view leaderboard and stats
         this.endGameStartTime = null;
-        this.endGameDuration = 2000;
+        this.endGameDuration = 20000;
         
         // 30 second voting time
         // this.votingDuration = 3000000;
